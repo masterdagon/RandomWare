fe9250f18bc0fc7b334108e561d54bf0c3106af4
masterdagon <masterdagon@hotmail.com>
2015-11-16 17:02:41 +0100

    initial commit

---
+++ app/src/main/java/com/example/muggi/randombebop/ListNotes.java
@@ -0,0 +1,34 @@
+package com.example.muggi.randombebop;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Muggi on 16-11-2015.
+ */
+public class ListNotes {
+
+    public ArrayList<String[]> notes = new ArrayList();
+
+    public ListNotes(){
+    }
+
+    public void createListItem(String title, String note){
+        String[] array = new String[2];
+        array[0]=title;
+        array[1]=note;
+        this.notes.add(array);
+    }
+
+    public ArrayList<String[]> getNotes(){
+        return notes;
+    }
+
+    public ArrayList<String> getTitles(){
+        ArrayList<String> astr = new ArrayList();
+        for(int i = 0; i<notes.size();i++){
+            astr.add(notes.get(i)[1]);
+        }
+        return astr;
+    }
+
+}
---
+++ app/src/main/java/com/example/muggi/randombebop/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.muggi.randombebop;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText inputText;
+    public TextView showText;
+    public ListNotes listNotes;
+    public ListView notelist;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        inputText = (EditText)findViewById(R.id.noteInmput);
+        showText = (TextView)findViewById(R.id.responseText);
+        setContentView( R.layout.activity_main);
+        notelist = (ListView)findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,listNotes.getTitles());
+        notelist.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void saveNote(View view){
+        String str = inputText.getText().toString();
+        listNotes.createListItem("test",str);
+        showText.setText(str);
+
+    }
+}
